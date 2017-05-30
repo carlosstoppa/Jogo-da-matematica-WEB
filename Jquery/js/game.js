06257e37fcec4f9787fd392bd580cf6c88240344
carlosstoppa <carloshenriqueveiga10@hotmail.com>
2017-05-30 02:22:02 -0300

    Jogo usando elementos do Jquery
    Jogo feito utilizando HTML5, CSS e JavaScript e elementos de Drag and Drop do Jquery.

---
+++ Jquery/js/game.js
@@ -0,0 +1,8 @@
+
+
+
+// Inicia o jogo
+reset();
+var then = Date.now;
+// O metodo setInterval chama uma funçao ou avalia uma expressao em intervalos especificos, nesse caso em Milisegundos.
+setInterval(main, 1);
